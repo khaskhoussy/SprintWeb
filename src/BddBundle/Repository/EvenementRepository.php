ef3a7b4a095bb4ae7a4141382fa9445bf73c95f5
farahnouisser <nouisserfarah@gmail.com>
2019-12-05 00:33:04 +0100

    Changes to be committed:

--- src/BddBundle/Repository/EvenementRepository.php
+++ src/BddBundle/Repository/EvenementRepository.php
@@ -4,6 +4,7 @@
 namespace BddBundle\Repository;
 
 
+use BddBundle\Entity\Participation;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\NonUniqueResultException;
 use Doctrine\ORM\Query;
@@ -38,6 +39,25 @@ class EvenementRepository extends EntityRepository
             )
             ->setParameter('str', '%'.$str.'%')
             ->getResult();
+    }
+
+    public function checkParticipation($idUser,$idEvenement) {
+        return $this->getEntityManager()
+            ->createQuery(
+                'SELECT COUNT(p)
+                FROM BddBundle:Participation p
+                WHERE p.iduser = :user and p.idevenement = :evenement'
+            )
+            ->setParameter('user',$idUser)
+            ->setParameter('evenement',$idEvenement)
+            ->getSingleScalarResult();
+    }
+
+
+    public function top5(){
+        $query=$this->getEntityManager()
+            ->createQuery("select c from BddBundle:Evenement c ORDER BY c.nbrParticipants DESC ");
+        return $query->getResult();
     }
 
 }
